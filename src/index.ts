f4d904ec27c46124cb2cf47f38bb635346f42024
yanawalka <nicolasdiezfajardo@gmail.com>
2021-05-20 17:55:30 -0300

    Agregue ts intro

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,44 @@
 /*
     ===== Código de TypeScript =====
 */
+function sumar(a:number,b:number): number{
+return a+b;
+}
+
+const sumarFlecha=(a:number, b:number):number => {
+return a+b;
+}
+
+const multiplicar=(a:number, b?:number, c:number=2):number =>{
+    return a *  c;
+}
+// const resultado= multiplicar(3);
+// console.log(resultado);
+
+
+
+
+interface PersonajesLOL{
+    nombre:string;
+    pv:number;
+
+}
+
+const nuevoPersonaje: PersonajesLOL = {
+nombre: 'zona',
+pv: 80
+}
+
+// function curar ( personaje:PersonajesLOL, curarx:number): void{
+// personaje.nombre='ellita';
+// }
+function nombrecamb(personaje:PersonajesLOL, cambio:string, ph:number):void{
+
+personaje.nombre=cambio;
+personaje.pv += ph;
+}
+
+nombrecamb(nuevoPersonaje,'ellitaaaaaa',20);
+
+
+console.log(nuevoPersonaje);
